3b1acbd0087853d0036da27aec8e561dd58f7306
Lindsay Evans <linz@linz.id.au>
2010-04-09 10:01:59 +1000

    Doin it

---
+++ README.md
@@ -0,0 +1,15 @@
+# jQuery JS Enabled
+
+jQuery plugin to check whether the user's browser has JS enabled
+
+## Requirements
+[jQuery](http://jquery.com/) (tested with 1.4.2)
+
+## Usage
+
+	if($.jsEnabled()){
+		// Do stuff that requires JS
+	}
+
+## Notes
+Yes, I do realise it's pointless ;)
---
+++ jquery-jsenabled.js
@@ -0,0 +1,15 @@
+/**
+* @license
+* jQuery JS Enabled
+*
+* Copyright (c) 2010 Boomworks <http://boomworks.com.au/>
+* Author: Lindsay Evans
+* Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
+*/
+(function($) {
+
+	$.jsEnabled = function(){
+		return !!(new Boolean(NaN));
+	};
+
+})(jQuery);
